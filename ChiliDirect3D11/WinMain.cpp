f506853b34013f0565ed220c6186916cdd1af7ba
alex <alextaylor9897@gmail.com>
2022-11-26 22:25:46 +0000

    Got WndProc and the message loop working (tutorial #3).

--- ChiliDirect3D11/WinMain.cpp
+++ ChiliDirect3D11/WinMain.cpp
@@ -1,5 +1,20 @@
 #include <Windows.h>
 
+LRESULT CALLBACK WndProc(
+    HWND hWnd,
+    UINT msg,
+    WPARAM wParam,
+    LPARAM lParam ) {
+    switch ( msg ) {
+    case WM_CLOSE:
+        PostQuitMessage( 123 );
+        break;
+    default:
+        break;
+    }
+    return DefWindowProc( hWnd, msg, wParam, lParam );
+}
+
 int CALLBACK WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
@@ -10,7 +25,7 @@ int CALLBACK WinMain(
     WNDCLASSEX wc = { 0 };
     wc.cbSize = sizeof( wc );
     wc.style = CS_OWNDC;
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = WndProc;
     wc.cbClsExtra = 0;
     wc.cbWndExtra = 0;
     wc.hInstance = hInstance;
@@ -37,7 +52,17 @@ int CALLBACK WinMain(
         nullptr );
 
     ShowWindow( hWnd, SW_SHOW );
-    while ( true );
 
-    return 0;
+    MSG msg;
+    BOOL gResult;
+    while ( (gResult = GetMessage( &msg, nullptr, 0, 0 )) > 0 ) {
+        TranslateMessage( &msg );
+        DispatchMessage( &msg );
+    }
+
+    if ( gResult == -1 ) {
+        return -1;
+    } else {
+        return msg.wParam;
+    }
 }
